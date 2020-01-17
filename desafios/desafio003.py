b8f3e04ba402574ab85f3e18b860e3ce75b59bf0
Gandolfo <58044231+ogandolfo@users.noreply.github.com>
2020-01-17 15:11:15 -0300

    Delete desafio003.py

--- desafios/desafio003.py
+++
@@ -1,5 +0,0 @@
-n1 = float(input('Digite um valor: '))
-n2 = float(input('Digite outro valor: '))
-s = n1+n2
-# Mostre a soma dos valores flutuantes usando 'chaves' na função print
-print('A soma de {} e {} é igual a {}'.format(n1, n2, s))
