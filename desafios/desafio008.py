336e6f7da511e0e24d50f5129d6a824551106a3a
Gandolfo <58044231+ogandolfo@users.noreply.github.com>
2020-01-17 15:12:15 -0300

    Delete desafio008.py

--- desafios/desafio008.py
+++
@@ -1,5 +0,0 @@
-# Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros
-
-v = float(input(' Digite um valor em metros: '))
-
-print(f' \n Você digitou {v} m. \n \n Seus resultados serão: \n {v/1000} km, {v/100} hm, {v/10} dam, \n {v*10} dm, {v*100} cm, {v*1000} mm.')
