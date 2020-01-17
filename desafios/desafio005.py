a1ccdbce55776a0b4c2f5412446f08adf5df48ba
Gandolfo <58044231+ogandolfo@users.noreply.github.com>
2020-01-17 15:11:40 -0300

    Delete desafio005.py

--- desafios/desafio005.py
+++
@@ -1,8 +0,0 @@
-# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e o seu antecessor
-n = int(input('Digite um valor:'))
-a = n-1
-s = n+1
-print(' O antecessor de {} é: {}'.format(n, a))
-print(' O sucessor de {} é: {}'.format(n, s))
-
-print(f'\nAnalisando o valor {n}, seu antecessor é {n-1} e seu sucessor é {n+1}.')
