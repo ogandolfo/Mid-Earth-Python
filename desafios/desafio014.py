080d5c082735af882ca2cec00fe0090e83daa0fe
Gandolfo <58044231+ogandolfo@users.noreply.github.com>
2020-01-17 15:13:30 -0300

    Delete desafio014.py

--- desafios/desafio014.py
+++
@@ -1,5 +0,0 @@
-# Escreva um programa que converta uma temperatura digitada de Cº para Fº
-
-t = float(input('Digite uma temperatura em °C: '))
-
-print(f'A temperatura em Fahrenheit será de F° {((t*1.8)+32)}.')
