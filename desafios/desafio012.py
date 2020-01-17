1bff2c9dd54a00a02dbfe670f76bc7a1d9c7a904
Gandolfo <58044231+ogandolfo@users.noreply.github.com>
2020-01-17 15:13:05 -0300

    Delete desafio012.py

--- desafios/desafio012.py
+++
@@ -1,5 +0,0 @@
-# Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto
-
-pr = float(input(' Digite o preço do produto: R$'))
-
-print(f' O preço do produto era de R$ {pr:.2f}. Com o desconto de 5%, seu novo preço será R$ {(pr-(pr*(5/100))):.2f}.')
