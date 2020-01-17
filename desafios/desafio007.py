abdee40b0afdeac2159e83c4642424a6fb671d5d
Gandolfo <58044231+ogandolfo@users.noreply.github.com>
2020-01-17 15:12:01 -0300

    Delete desafio007.py

--- desafios/desafio007.py
+++
@@ -1,8 +0,0 @@
-# Desenvolva um programa que leia as duas notas de um aluno, calcule a mostre a sua média
-
-n1 = float(input(' Digite a primeira nota:'))
-n2 = float(input(' Digite a segunda nota: '))
-
-md = ((n1+n2)/2)
-
-print(f' Suas notas foram {n1} e {n2} respectivamente. Sua média é {md}')
