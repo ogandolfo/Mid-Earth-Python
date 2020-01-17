03ecc19bfbb42730af86ae46df202673c28c307b
Gandolfo <58044231+ogandolfo@users.noreply.github.com>
2020-01-17 15:16:05 -0300

    Delete aula07b.py

--- aulas/aula07b.py
+++
@@ -1,5 +0,0 @@
-nome = str(input('Qual é o seu nome? '))
-print('Prazer em te conhecer, {:<20}!'.format(nome))
-print('Prazer em te conhecer, {:>20}!'.format(nome))
-print('Prazer em te conhecer, {:^20}!'.format(nome))
-print('Prazer em te conhecer, {:=^20}!'.format(nome))
