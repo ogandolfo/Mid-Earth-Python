7d90dc0f7fb93421b9aa119c2eb94b0d53ff4351
Gandolfo <58044231+ogandolfo@users.noreply.github.com>
2020-10-24 16:18:04 -0300

    Delete desafio030.py

--- Mundo 1/desafios/desafio030.py
+++
@@ -1,8 +0,0 @@
-# Crie um programa que leia um número e mostre na tela se ele é par ou ímpar
-
-n = float(input('Digite um número e descubra se ele é par ou impar: '))
-
-if float(n) % 2 == 0:  # se o número é divisível por 2, o resto é zero, sendo assim, é par
-    print(f'Você digitou {n}, que é um número par')
-else:
-    print(f'Você digitou {n}, que é um número ímpar')
