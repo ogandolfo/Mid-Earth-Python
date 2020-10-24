fd71e8cbd7b75c878fbecc7806724ddc079f5fec
Gandolfo <58044231+ogandolfo@users.noreply.github.com>
2020-10-24 16:11:54 -0300

    Delete aula10b.py

--- Mundo 1/aulas/aula10b.py
+++
@@ -1,8 +0,0 @@
-# condições (if/else)
-
-nome = str(input('Digite seu nome? '))
-if nome == 'Gabriel':
-    print('Que nome lindo você tem!')
-else:
-    print('Seu nome é tão normal...')
-print(f'Bom dia, {nome}.')
