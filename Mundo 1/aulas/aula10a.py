a0873c2c69856f42a5e8afd6d08b98acf8d64417
Gandolfo <58044231+ogandolfo@users.noreply.github.com>
2020-10-24 16:11:50 -0300

    Delete aula10a.py

--- Mundo 1/aulas/aula10a.py
+++
@@ -1,9 +0,0 @@
-# condições (if/else)
-
-tempo = int(input('Quantos anos tem seu carro?'))
-if tempo <=3:
-    print('Carro novo')
-else:
-    print('Carro Velho')
-print('--FIM--')
-print('Carro novo' if tempo <=3 else 'Carro Velho')
