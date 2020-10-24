e6b54ffc10cbebd73f58ec66b893da0d25405b28
Gandolfo <58044231+ogandolfo@users.noreply.github.com>
2020-10-24 16:17:32 -0300

    Delete desafio025.py

--- Mundo 1/desafios/desafio025.py
+++
@@ -1,5 +0,0 @@
-# Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome
-
-n1 = str(input('Digite seu nome: ')).strip().title()
-n2 = 'Silva' in n1
-print(f'Seu nome tem Silva? {n2}')
