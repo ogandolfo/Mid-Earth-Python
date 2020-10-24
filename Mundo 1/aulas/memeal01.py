549d90c85585a6634250c62e6d69441309be577c
Gandolfo <58044231+ogandolfo@users.noreply.github.com>
2020-10-24 16:14:34 -0300

    Delete memeal01.py

--- Mundo 1/aulas/memeal01.py
+++
@@ -1,7 +0,0 @@
-n = int(input('\nDescubra se Rodorfo está morto. Digite 1 para sim ou 2 para não: '))
-
-if n <= 1:
-    print('\nRodorfooooo!!!')
-else:
-    print('\nAh, estava dormindo. Que susto rsrs...')
-
