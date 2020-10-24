ff210c369a6ce86c0e33cd902c48a03ae34dd159
Gandolfo <58044231+ogandolfo@users.noreply.github.com>
2020-10-24 16:16:15 -0300

    Delete desafio010.py

--- Mundo 1/desafios/desafio010.py
+++
@@ -1,7 +0,0 @@
-# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar
-
-cart = float(input(' Quanto dinheiro você tem? R$'))
-dol = cart / 4.205
-bc = cart / 34964.14
-print(f'\nVocê possui R$ {cart:.2f}, podendo comprar US$ {dol:.2f}.')
-print(f'\nVocê também pode comprar {bc:.2f} bitcoins.')
